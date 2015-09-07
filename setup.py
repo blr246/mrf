adb970c306d0bdb663f84b06ead97e403631020a
Brandon L. Reiss <brandon.l.reiss@gmail.com>
2015-09-07 14:37:02 -0400

    Convert to tox project.
    Add tox.ini, setup.py, and other essential artifacts for project
    distrubution.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+
+setup(
+    name='mrf',
+    version='0.1',
+    description='Markov Random Field library',
+    author='Brandon L. Reiss',
+    author_email='brandon@brandonreiss.com',
+    url='https://github.com/blr246/mrf',
+    packages=find_packages(),
+)
